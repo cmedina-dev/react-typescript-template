51be3fadc5442d986d8bc7019413b82bd54a37b9
cmedina-dev <christian.medina.atx@gmail.com>
2024-01-16 11:54:24 -0800

    Fix package.json configs

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import './styles/index.scss';
 
 // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-            <React.StrictMode>
+  <React.StrictMode>
     <App />
   </React.StrictMode>,
 );
